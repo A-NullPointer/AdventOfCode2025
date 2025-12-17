68028ca02fd3ddd7612df70e639c6010acf7f9a9
A-rar <asmaeezzaim404@gmail.com>
2025-12-17 15:27:23 +0000

    feat: Modeled the day 01 code logic and added the input file to resources

---
+++ src/main/java/software/aoc/day01/a/model/Direction.java
@@ -0,0 +1,9 @@
+package software.aoc.day01.a.model;
+
+public enum Direction{
+    L( -1),
+    R( + 1);
+
+    Direction(int operation) {
+    }
+}
---
+++ src/main/java/software/aoc/day01/a/model/Rotation.java
@@ -0,0 +1,4 @@
+package software.aoc.day01.a.model;
+
+public record Rotation(Direction direction, int times) {
+}
